0f2dcc5127135627ef131afcbbea0149fad2f3bd
Alvaro Valera <alvarovaleralopez@gmail.com>
2022-11-29 01:00:06 +0100

    Completamos el diseño inicial css y añadimos el script al código.

---
+++ js/formValidations.js
@@ -0,0 +1,7 @@
+/* OBTENEMOS EL VALOR DEL INPUT NICK */
+const nickInput = document.getElementById('nick');
+console.log(nickInput.value);
+
+/* OBTENEMOS EL VALOR DEL SELECT. */
+const tamnioJuego = document.getElementById('tamanio');
+console.log(`El valor del campo select es: ${tamnioJuego.value}, que corresponde a: ${tamnioJuego.options[tamnioJuego.selectedIndex].text}.`);
